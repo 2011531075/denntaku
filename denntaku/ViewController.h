c90f74cebda1b8814638cfcd6f849358b7c7ae38
Firstname Lastname <s1531075@eng.kitakyu-u.ac.jp>
2013-11-21 16:03:24 +0900

    ボタンの登録

--- denntaku/ViewController.h
+++ denntaku/ViewController.h
@@ -10,4 +10,37 @@
 
 @interface ViewController : UIViewController
 
+
+@property (weak, nonatomic) IBOutlet UILabel *gamenn;
+
+@property (weak, nonatomic) IBOutlet UIButton *ACbotann;
+@property (weak, nonatomic) IBOutlet UIButton *purasutomainasubotann;
+@property (weak, nonatomic) IBOutlet UIButton *pasenntobotann;
+@property (weak, nonatomic) IBOutlet UIButton *warubotann;
+@property (weak, nonatomic) IBOutlet UIButton *nanabotann;
+@property (weak, nonatomic) IBOutlet UIButton *hatibotann;
+@property (weak, nonatomic) IBOutlet UIButton *kyuubotann;
+@property (weak, nonatomic) IBOutlet UIButton *kakerubotann;
+@property (weak, nonatomic) IBOutlet UIButton *yonnbotann;
+@property (weak, nonatomic) IBOutlet UIButton *gobotann;
+@property (weak, nonatomic) IBOutlet UIButton *rokubotann;
+@property (weak, nonatomic) IBOutlet UIButton *mainasubotann;
+@property (weak, nonatomic) IBOutlet UIButton *itibotann;
+@property (weak, nonatomic) IBOutlet UIButton *nibotann;
+@property (weak, nonatomic) IBOutlet UIButton *sannbotann;
+@property (weak, nonatomic) IBOutlet UIButton *purasubotann;
+@property (weak, nonatomic) IBOutlet UIButton *zerobotann;
+@property (weak, nonatomic) IBOutlet UIButton *tennbotann;
+@property (weak, nonatomic) IBOutlet UIButton *ikorubotann;
+@property (weak, nonatomic) IBOutlet UILabel *gamennraberu;
+
+
+
+
+- (IBAction)numberButton:(id)sender;
+
+- (IBAction)pushACbotann:(id)sender;
+
+
+
 @end
